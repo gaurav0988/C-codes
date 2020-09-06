b98b2d6334ebd8147b79b3b15ef1ac66f05148e0
gaurav0988 <33927608+gaurav0988@users.noreply.github.com>
2020-09-06 23:03:21 +0530

    Add files via upload

---
+++ adding floating point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 34.23;
+
+            double y = 45.76;
+            double z = x + y;
+            Console.WriteLine("z =" + z);
+        }
+
+        }
+}
